de6aa8a49b8451f33ae181daa10f9c01e7b27850
slava-spb <slava3@gmail.com>
2017-01-12 00:04:45 +0300

    php

--- php/task_less1.php
+++ php/task_less1.php
@@ -1,11 +1,14 @@
 <?php
   $time = date("i");
   $a = $time%5;
-  echo $a <= 2 ? 'зелёный' : 'Красный' . "\n";
+  echo $a <= 2 ? 'зелёный' . "\n" : 'Красный' . "\n";
 
 
-  $SuchSot = 10;
-  $SuchMetrKv = $SuchSot*100;
-  echo $SuchMetrKv . "\n";
-  $ogorod = 15*25;
-    echo $ogorod;
+    $SuchSot = 10;
+    $SuchMetrKv = $SuchSot*100;
+    echo $SuchMetrKv . "\n";
+    $ogorod = 15*25;
+    echo $ogorod ."\n";
+    $b=$SuchMetrKv%$ogorod;
+    echo $b;
+
